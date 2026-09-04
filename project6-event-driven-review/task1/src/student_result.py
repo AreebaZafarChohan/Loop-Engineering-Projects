5897252cc120d0f19b2e85c01ef1efc77caef768
AreebaZafarChohan <areebazafar715@gmail.com>
2026-09-04 08:25:41 +0500

    Add project 6 event-driven review task

---
+++ project6-event-driven-review/task1/src/student_result.py
@@ -0,0 +1,11 @@
+def calculate_grade(average):
+    if average >= 90:
+        return "A"
+    elif average >= 80:
+        return "B"
+    elif average >= 70:
+        return "C"
+    elif average >= 60:
+        return "D"
+    else:
+        return "F"
