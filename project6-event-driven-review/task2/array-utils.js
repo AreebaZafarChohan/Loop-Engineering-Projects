9fced32ebad82622a338add6becfec28db2937b6
AreebaZafarChohan <areebazafar715@gmail.com>
2026-09-04 15:44:55 +0500

    Update array bounds checking

--- project6-event-driven-review/task2/array-utils.js
+++ project6-event-driven-review/task2/array-utils.js
@@ -1,8 +1,5 @@
 function getItemAt(arr, index) {
-  if (arr === null || arr === undefined) {
-    return undefined;
-  }
-  if (index < 0 || index >= arr.length) {
+  if (index < 0 || index > arr.length) {
     return undefined;
   }
   return arr[index];
